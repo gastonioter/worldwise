abf6fdfa4d57f61f62dda4f43e72acda43f0ef25
Gaston <gaston10.c@hotmail.com>
2024-02-23 17:12:32 -0300

    routes definition

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import NotFound from "./pages/NotFound";
+
 export default function App() {
-  return <div>World Wise</div>;
+  return (
+    <div>
+      World Wise App!
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <div>Sorry!,The page doesent exists ): </div>;
+}
