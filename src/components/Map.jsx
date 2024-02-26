1f90fe752f2a48b465e6c5dddf6340742d4956ad
gastonioter <iotting.castro10@gmail.com>
2024-02-26 18:42:07 -0300

    useGeolocation hook

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,51 +4,109 @@
 /* eslint-disable no-unused-vars */
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import Spinner from "./Spinner";
 import {
   APIProvider,
+  ControlPosition,
   InfoWindow,
+  useMapsLibrary,
   Map,
+  MapControl,
   Marker,
+  useMap,
   useMarkerRef,
 } from "@vis.gl/react-google-maps";
 import { useCities } from "../contexts/CityContext";
-import { useState } from "react";
-const GOOGLE_MAPS_API_KEY = "AIzaSyDNRMow6Cv6ifnFdq8SwXy-HQ2yF4Rkauk";
+import { useEffect, useState } from "react";
+import { useGeolocation } from "../hooks/useGeolocation";
+const API_KEY = "AIzaSyDNRMow6Cv6ifnFdq8SwXy-HQ2yF4Rkauk";
 
-const center = { lat: -34, lng: -60 };
 export default function Maps() {
   const { cities } = useCities();
+  const navigate = useNavigate();
+  const geocodeLib = useMapsLibrary("geocoding");
+
+  const {
+    getPosition,
+    position,
+    isLoading: isGeolocating,
+  } = useGeolocation({
+    lat: -50,
+    lng: 0,
+  });
+
+  function onMapClick(e) {
+    const { lat, lng } = e.detail.latLng;
+    console.log(geocodeLib);
+    navigate(`form?lat=${lat}&lng=${lng}`);
+  }
 
   return (
     <div className={styles.mapContainer}>
-      <APIProvider apiKey={GOOGLE_MAPS_API_KEY} className={styles.mapContainer}>
+      <APIProvider apiKey={API_KEY}>
         <Map
-          minZoom={3}
-          defaultZoom={8}
-          defaultCenter={center}
-          styles={[
-            {
-              featureType: "poi",
-              elementType: "labels",
-              stylers: [{ visibility: "off" }],
-            },
-            {
-              featureType: "transit",
-              elementType: "labels.icon",
-              stylers: [{ visibility: "off" }],
-            },
-          ]}
+          defaultCenter={position}
+          defaultZoom={4}
+          disableDefaultUI
+          onClick={onMapClick}
         >
-          {cities.map((city) => (
-            <MarkerWithInfo key={city.id} city={city} />
+          <MapControl position={ControlPosition.BOTTOM_CENTER}>
+            <div>
+              {" "}
+              <button
+                onClick={getPosition}
+                className={`btnMap ${isGeolocating ? "btnLoading" : ""}`}
+              >
+                {isGeolocating ? "Loading..." : "USE YOUR LOCATION"}
+              </button>{" "}
+            </div>
+          </MapControl>
+
+          {cities.map((c) => (
+            <MarkerWithInfo city={c} key={c.id}></MarkerWithInfo>
           ))}
         </Map>
+
+        <MapController position={position} />
       </APIProvider>
     </div>
   );
 }
 
+function MapController({ position }) {
+  const map = useMap();
+  const [mapPosition, setMapPosition] = useState({ lat: -50, lng: -0 });
+  const [searchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  // Synchronize with URL params
+  useEffect(
+    function updateMapCenter() {
+      if (!lat || !lng) return;
+      setMapPosition({ lat: +lat, lng: +lng });
+    },
+    [lat, lng]
+  );
+
+  // Synchronize with geolocation api
+  useEffect(
+    function () {
+      setMapPosition(position);
+    },
+    [position]
+  );
+
+  useEffect(
+    function () {
+      if (!map) return;
+
+      map.panTo(mapPosition);
+    },
+    [map, mapPosition]
+  );
+
+  return <></>;
+}
 function MarkerWithInfo({ city }) {
   const [markerRef, marker] = useMarkerRef();
   const [isMarkerOpen, setIsMarkerOpen] = useState(true);
@@ -64,7 +122,7 @@ function MarkerWithInfo({ city }) {
         <InfoWindow
           onCloseClick={() => setIsMarkerOpen(false)}
           anchor={marker}
-          content={` <h3 class=popup-content>${city.emoji} ${city.cityName}</h3>`}
+          content={` <h2 class=popup-content>${city.emoji} ${city.cityName}</h2>`}
         ></InfoWindow>
       )}
     </>
--- src/hooks/useGeolocation.js
+++ src/hooks/useGeolocation.js
@@ -1,30 +1,25 @@
-/* eslint-disable no-unused-vars */
 import { useState } from "react";
 
-export function useGeolocation() {
+export function useGeolocation(defaultPosition) {
   const [isLoading, setIsLoading] = useState(false);
-  const [position, setPosition] = useState({});
+  const [position, setPosition] = useState(defaultPosition);
   const [error, setError] = useState(null);
 
   function getPosition() {
-    if (!navigator.geolocation) return;
-    setError("Geolocation is not supported");
-
     setIsLoading(true);
-    navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude: lat, longitude: lng } = position.coords;
+
+        setIsLoading(false);
+        setPosition({ lat, lng });
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
   }
 
-  function handleSuccess(position) {
-    const { latitude, longitude } = position.coords;
-    setPosition({ latitude, longitude });
-    setIsLoading(false);
-  }
-
-  function handleError(error) {
-    setError(error.message);
-    setIsLoading(false);
-
-  }
-
-  return { isLoading, position, error, getPosition };
+  return { getPosition, isLoading, error, position };
 }
