4dd80aff4e2f334b7d22f05bb5136b9fa9271296
gastonioter <iotting.castro10@gmail.com>
2024-02-26 02:42:05 -0300

    paint a marker for each city

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,17 +1,27 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 import Spinner from "./Spinner";
-import { APIProvider, Map, Marker, useMap } from "@vis.gl/react-google-maps";
+import {
+  APIProvider,
+  InfoWindow,
+  Map,
+  Marker,
+  useMarkerRef,
+  useMap,
+} from "@vis.gl/react-google-maps";
 import { useEffect, useState } from "react";
+import { useCities } from "../contexts/CityContext";
 const GOOGLE_MAPS_API_KEY = "AIzaSyDNRMow6Cv6ifnFdq8SwXy-HQ2yF4Rkauk";
 
 function MyComponent() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const map = useMap();
 
   // const lat = searchParams.get("lat");
   // const lng = searchParams.get("lng");
-  const map = useMap();
 
   useEffect(
     function () {
@@ -27,6 +37,7 @@ function MyComponent() {
 export default function Maps() {
   const [userCoords, setUserCoords] = useState({ lat: 0, lng: 0 });
   const navigate = useNavigate();
+  const { cities, isLoading } = useCities();
 
   useEffect(function onLoad() {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -39,12 +50,14 @@ export default function Maps() {
     });
   }, []);
 
-  if (userCoords.lat === 0 && userCoords.lng === 0)
+  if ((userCoords.lat === 0 && userCoords.lng === 0) || isLoading)
     return (
       <div className={styles.mapContainer}>
         <Spinner />
       </div>
     );
+
+  console.log("map is on the screen");
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
@@ -55,7 +68,7 @@ export default function Maps() {
       >
         <Map
           minZoom={3}
-          defaultZoom={10}
+          defaultZoom={2}
           defaultCenter={userCoords}
           styles={[
             {
@@ -69,10 +82,32 @@ export default function Maps() {
               stylers: [{ visibility: "off" }],
             },
           ]}
-        />
+        >
+          {cities.map((city) => (
+            <MyMarker key={city.id} city={city} />
+          ))}
+        </Map>
 
         <MyComponent />
       </APIProvider>
     </div>
+  );
+}
+
+function MyMarker({ city }) {
+  const [markerRef, marker] = useMarkerRef();
+
+  return (
+    <>
+      <Marker
+        ref={markerRef}
+        position={{ lat: city.position.lat, lng: city.position.lng }}
+      />
+
+      <InfoWindow
+        anchor={marker}
+        content={` <h1 class=popup-content>${city.emoji} ${city.cityName}</h1>`}
+      ></InfoWindow>
+    </>
   );
 }
