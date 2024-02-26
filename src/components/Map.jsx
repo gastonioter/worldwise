f7b0610b7b5fe29815e8051ae028e3be81280d5b
gastonioter <iotting.castro10@gmail.com>
2024-02-26 17:20:38 -0300

    refactor Map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,66 +11,22 @@ import {
   Map,
   Marker,
   useMarkerRef,
-  useMap,
 } from "@vis.gl/react-google-maps";
-import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CityContext";
-import { useGeolocation } from "../hooks/useGeolocation";
+import { useState } from "react";
 const GOOGLE_MAPS_API_KEY = "AIzaSyDNRMow6Cv6ifnFdq8SwXy-HQ2yF4Rkauk";
 
-function MyComponent() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const map = useMap();
-
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
-
-  useEffect(
-    function () {
-      if (!map) return;
-      if (!lat || !lng) return;
-
-      map.panTo({ lat: parseFloat(lat), lng: parseFloat(lng) });
-      map.setZoom(8);
-    },
-
-    [map, lat, lng]
-  );
-
-  return <></>;
-}
-
+const center = { lat: -34, lng: -60 };
 export default function Maps() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const { getPosition, isLoading: isLoadingGeo, position } = useGeolocation();
-
-  const navigate = useNavigate();
-
   const { cities } = useCities();
-
-  useEffect(function () {
-    getPosition();
-  }, []);
-
-  if (isLoadingGeo)
-    return (
-      <div className={styles.mapContainer}>
-        <Spinner />
-      </div>
-    );
-
-  function handleOnClick(e) {
-    const { lat, lng } = e.detail.latLng;
-    navigate(`form?lat=${lat}&lng=${lng}`);
-  }
 
   return (
     <div className={styles.mapContainer}>
       <APIProvider apiKey={GOOGLE_MAPS_API_KEY} className={styles.mapContainer}>
         <Map
           minZoom={3}
-          defaultZoom={2}
-          defaultCenter={position}
+          defaultZoom={8}
+          defaultCenter={center}
           styles={[
             {
               featureType: "poi",
@@ -83,37 +39,34 @@ export default function Maps() {
               stylers: [{ visibility: "off" }],
             },
           ]}
-          onClick={handleOnClick}
         >
           {cities.map((city) => (
-            <MyMarker key={city.id} city={city} />
+            <MarkerWithInfo key={city.id} city={city} />
           ))}
         </Map>
-
-        <MyComponent />
       </APIProvider>
     </div>
   );
 }
 
-function MyMarker({ city }) {
+function MarkerWithInfo({ city }) {
   const [markerRef, marker] = useMarkerRef();
-
-  function handleMarkerClick(e) {
-    // show info window
-  }
+  const [isMarkerOpen, setIsMarkerOpen] = useState(true);
   return (
     <>
       <Marker
         ref={markerRef}
-        onClick={handleMarkerClick}
+        onClick={() => setIsMarkerOpen(true)}
         position={{ lat: city.position.lat, lng: city.position.lng }}
       />
 
-      <InfoWindow
-        anchor={marker}
-        content={` <h1 class=popup-content>${city.emoji} ${city.cityName}</h1>`}
-      ></InfoWindow>
+      {isMarkerOpen && (
+        <InfoWindow
+          onCloseClick={() => setIsMarkerOpen(false)}
+          anchor={marker}
+          content={` <h3 class=popup-content>${city.emoji} ${city.cityName}</h3>`}
+        ></InfoWindow>
+      )}
     </>
   );
 }
